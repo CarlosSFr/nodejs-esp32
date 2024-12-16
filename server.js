a9ad4d7f7343f0a44787edcecb3e9329ff6d397b
Carlos Eduardo Santos Freitas <carlos.freitas@valmetpartners.com>
2024-12-16 19:38:35 -0300

    Adding mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+require("dotenv").config()
 const app = express()
 
 app.use(bodyParser.json())
 const api = require("./routes/") // aqui estou pegando o "index.js", não está referenciado pq index é padrão
 app.use("/api", api) // funciona como um middleware, linkando as rotas à api
-const PORT = 3000
-
+const PORT = process.env.PORT
 
 app.get("/", (req, res) => {
     res.json({
